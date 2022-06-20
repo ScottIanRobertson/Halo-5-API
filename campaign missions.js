2069fe4ac8f86d3c937ef7f3e80617eb02c69a3f
Scott Robertson <50124818+ScottIanRobertson@users.noreply.github.com>
2022-06-20 16:53:13 +1000

    Add files via upload

---
+++ campaign missions.js
@@ -0,0 +1,138 @@
+window.addEventListener("load", function() {
+  let finalURL2 = `https://www.haloapi.com/metadata/h5/metadata/campaign-missions`;
+  console.log(finalURL2);
+  fetch(finalURL2, {
+    method: "GET",
+    headers: {
+      "Ocp-Apim-Subscription-Key": "b739d44798694b3bb7c2c35748b2f575"
+    }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      campaignresult.innerHTML = `
+      <h2>${data[0].missionNumber}. ${data[0].name}</h2>
+      <img src="${data[0].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[0].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[1].missionNumber}. ${data[1].name}</h2>
+      <img src="${data[1].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[1].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[2].missionNumber}. ${data[2].name}</h2>
+      <img src="${data[2].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[2].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[3].missionNumber}. ${data[3].name}</h2>
+      <img src="${data[3].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[3].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[4].missionNumber}. ${data[4].name}</h2>
+      <img src="${data[4].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[4].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[5].missionNumber}. ${data[5].name}</h2>
+      <img src="${data[5].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[5].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[6].missionNumber}. ${data[6].name}</h2>
+      <img src="${data[6].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[6].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[7].missionNumber}, ${data[7].name}</h2>
+      <img src="${data[7].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[7].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[8].missionNumber}. ${data[8].name}</h2>
+      <img src="${data[8].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[8].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[9].missionNumber}. ${data[9].name}</h2>
+      <img src="${data[9].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[9].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[10].missionNumber}. ${data[10].name}</h2>
+      <img src="${data[10].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[10].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[11].missionNumber}. ${data[11].name}</h2>
+      <img src="${data[11].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[11].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[12].missionNumber}. ${data[12].name}</h2>
+      <img src="${data[12].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[12].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[13].missionNumber}. ${data[13].name}</h2>
+      <img src="${data[13].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[13].description}</span>
+            </div>
+        </div>
+
+        <h2>${data[14].missionNumber}. ${data[14].name}</h2>
+      <img src="${data[14].imageUrl}" class="mission-img">
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <span>${data[14].description}</span>
+            </div>
+        </div>
+        `
+
+    })
+    .catch(() => {
+        campaignresult.innerHTML = `<h3>Catch</h3>`;
+    });
+});
---
+++ servicetag.js
@@ -0,0 +1,46 @@
+let searchBtn = document.getElementById("search-btn");
+let gamerInp = document.getElementById("gamer-inp");
+gamerInp.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    searchBtn.click();
+  }
+});
+searchBtn.addEventListener("click", () => {
+  new KeyboardEvent("keydown", { key: "Enter"});
+  let player = gamerInp.value;
+  let finalURL = `https://www.haloapi.com/profile/h5/profiles/${player}/appearance`;
+  console.log(finalURL);
+  fetch(finalURL, {
+    method: "GET",
+    headers: {
+      "Ocp-Apim-Subscription-Key": "b739d44798694b3bb7c2c35748b2f575"
+    }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      servicetagresult.innerHTML = `
+
+        <h2>${data.Gamertag}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>ServiceTag:</h4>
+                <span>${data.ServiceTag}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Company:</h4>
+                <span>${data.Company.Name}</span>
+            </div>
+        </div> `
+
+    })
+    .catch(() => {
+      if (player.length == 0) {
+        servicetagresult.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        servicetagresult.innerHTML = `<h3>Please enter a valid Halo 5 gamertag.</h3>`;
+      }
+    });
+});
+
